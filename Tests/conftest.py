ddf92026e233ed4d14bca214adbec3cca62ba599
Sergey Ikonnikov <s.ikonnikov@profeelab.net>
2025-02-17 12:31:12 +0800

    init

---
+++ Tests/conftest.py
@@ -0,0 +1,28 @@
+import os
+
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+# from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_conf():
+    browser.config.window_height = 800
+    browser.config.window_width = 1280
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless')
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture
+def file_path():
+    return os.path.join(os.path.dirname(__file__), 'Files', 'meme.png')
---
+++ Tests/test_demoqa.py
@@ -0,0 +1,44 @@
+from selene import browser, have, be
+
+
+def test_complete_form(browser_conf, file_path):
+    browser.open('/')
+    browser.element('#app').should(be.visible)
+    browser.element('#app').should(have.text('Practice Form'))
+    browser.element('#fixedban').should(be.visible)
+    browser.driver.execute_script("$('#fixedban').remove()")
+    browser.driver.execute_script("$('footer').remove()")
+    browser.driver.execute_script("$('header').remove()")
+    browser.element('#firstName').type(text="Sergey")
+    browser.element('#lastName').type(text="Labov")
+    browser.element('#userEmail').type(text="qaguru@nosuchdomain.net")
+    browser.element('[for="gender-radio-1"]').click()
+    browser.element('#userNumber').type(text="9111002030")
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('select').element('option[value="7"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('option[value="1989"]').click()
+    browser.element('.react-datepicker__day--017').click()
+    browser.element('#subjectsInput').type(text="English").press_enter()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('[for="hobbies-checkbox-3"]').click()
+    browser.element('input[type="file"]').set_value(file_path)
+    browser.element('#currentAddress').type(text='First street, Second house, Third app.')
+    browser.element('#state').click()
+    browser.element('#react-select-3-option-3').click()
+    browser.element('#city').click()
+    browser.element('#react-select-4-option-1').click()
+    browser.element('#submit').click()
+    browser.element('.table').all('td:nth-child(2)').should(have.exact_texts(
+        "Sergey Labov",
+        "qaguru@nosuchdomain.net",
+        "Male",
+        "9111002030",
+        "17 August,1989",
+        "English",
+        "Sports, Reading, Music",
+        "meme.png",
+        "First street, Second house, Third app.",
+        "Rajasthan Jaiselmer"))
